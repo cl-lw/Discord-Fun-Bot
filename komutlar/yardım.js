1849cf8482a420a521531a1c16a6172166ccd9a5
cl_lw <68690911+cllw-code@users.noreply.github.com>
2023-02-26 13:48:05 +0300

    Add files via upload

---
+++ komutlar/yardım.js
@@ -0,0 +1,82 @@
+const Discord = require('discord.js')
+const ayarlar = require('../ayarlar.json') 
+
+exports.run = async (client, message, args) => {
+    var prefix = ayarlar.prefix;
+
+
+    //SUNUCU KOMUTU
+      if(args[0] === "Sunucu" || args[0] === "sunucu") {
+              let Sunucu = new Discord.MessageEmbed()
+  .setAuthor('Sunucu', message.author.displayAvatarURL())
+  .setColor('#b448e6')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Sunucu').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+      
+              return message.channel.send(Sunucu)
+         
+      
+
+       return;
+    }
+//EĞLENCE KOMUTU
+  if(args[0] === "Eğlence" || args[0] === "eğlence" || args[0] === "Fun" || args[0] === "fun") {
+   let Eğlence = new Discord.MessageEmbed()
+  .setAuthor('Eğlence', message.author.displayAvatarURL())
+  .setColor('#b448e6')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Eğlence').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+      
+   return message.channel.send(Eğlence)
+  
+      
+           return;
+  }
+  //MODERASYON KOMUTU
+  if(args[0] === "Moderasyon" || args[0] === "moderasyon" || args[0] === "moderation" || args[0] === "Moderation") {
+   let Moderasyon = new Discord.MessageEmbed()
+  .setAuthor('Moderasyon', message.author.displayAvatarURL())
+  .setColor('#b448e6')
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+   .setDescription(client.commands.filter(cmd => cmd.conf.kategori === 'Moderasyon').map(cmd => `:white_small_square: - **${prefix}${cmd.help.name}** ${cmd.help.description}`).join("\n "))
+      
+   return message.channel.send(Moderasyon)
+             
+       
+               return;
+  }
+
+//YARDIM KOMUTU
+  
+  let embed = new Discord.MessageEmbed()
+  .setAuthor('Yardım Komutları', message.author.displayAvatarURL())
+  .setThumbnail(client.user.avatarURL())
+  .setColor('#b448e6')
+  .setDescription(`**Örnek Kullanım:** \`${prefix}help Eğlence\` \n **Örnek:** \`${prefix}help Eğlence\``)
+  .addField('Kategoriler:', `
+  **[${prefix}help Eğlence]()**
+
+  
+  `)
+  
+
+  .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
+  message.channel.send(embed)
+
+  
+}
+    
+
+
+  exports.conf = {
+    aliases: ['help', 'cmds', 'komutlar','y'], 
+    permLevel: 0, 
+    kategori: "Genel" 
+
+  };
+
+  exports.help = {
+    name: 'yardım',  
+    description: 'Komutlar hakkında bilgi verir.', 
+    usage: 'yardım',
+  };
