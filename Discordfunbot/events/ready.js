bd89163da997ff33471f90e78b45c7a7954d5313
cl_lw <68690911+cllw-code@users.noreply.github.com>
2023-02-26 13:47:34 +0300

    Delete Discordfunbot directory

--- Discordfunbot/events/ready.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-const ayarlar = require('../ayarlar.js');
-client.on("ready ")
-module.exports = async client => {
-  client.user.setPresence({ activity: { type: "WATCHING", name: `!help`}, status: 'dnd' })
-};
-
